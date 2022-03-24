4f6670f1f1590117333dd4c1b0387869c3477185
Apoh <Edenra08@gmail.com>
2022-03-24 18:39:17 +0000

    commit tree d209ee9188bd1438be4f08fbbff97120e4a09805

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){ 
+    return `This Monday, I will ${activity}.`   
+}
+
+mondayWork("work from home")
+
+
+
+
+
+// it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
+//     let result = wrapAdjective('*')
+//     let emphatic = result("a hard worker")
+//     expect(emphatic).to.equal("You are *a hard worker*!")
+//   });
+
+
+function wrapAdjective( string){
+   
+    return function (){
+
+    }
+
+}
+
+
+//let result = '*';
+//  let emphatic = result("a hard worker")
